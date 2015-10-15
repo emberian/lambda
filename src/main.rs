34b963b162268322cc5bbc3d84d4bbe96e6e110e
Corey Richardson <corey@octayn.net>
2015-10-15 18:30:57 -0400

    Fancy repl prompt

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,23 @@ mod ast;
 mod eval;
 mod parser;
 
-use std::io::BufRead;
+use std::io::prelude::*;
 
 fn main() {
     let stdin = std::io::stdin();
-    for line in stdin.lock().lines() {
-        let line = line.unwrap();
-        let t = match parser::parse_Term(line.trim()) {
+    let mut stdout = std::io::stdout();
+    let mut s = String::new();
+    loop {
+        stdout.write("λ ".as_bytes()).unwrap();
+        stdout.flush().unwrap();
+        stdin.read_line(&mut s).unwrap();
+        let t = match parser::parse_Term(s.trim()) {
             Ok(t) => t,
             Err(e) => { println!("Parse error: {:?}", e); continue }
         };
         println!("big_eval = {}, small_eval = {}",
                  match eval::big_eval(&*t) { Ok(o) => format!("{}", o), Err(e) => format!("{}", e) },
                  eval::small_eval(&*t));
+        s.clear();
     }
 }
